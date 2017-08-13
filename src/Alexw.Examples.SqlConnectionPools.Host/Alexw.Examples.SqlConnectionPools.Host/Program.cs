1a3e193643270a3c112aa3c26576b7ea6b319789
Alexander Williamson <Alexander Williamson>
2017-08-13 19:35:06 +0100

    Add migrations and fix proj

---
+++ src/Alexw.Examples.SqlConnectionPools.Host/Alexw.Examples.SqlConnectionPools.Host/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alexw.Examples.SqlConnectionPools.Host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
